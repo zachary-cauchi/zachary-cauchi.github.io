5dcbccebefa6850f42e34b954314e495be7f311f
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-06-27 21:08:42 +0200

    feat: add initial template

---
+++ pages/resume.vue
@@ -0,0 +1,63 @@
+<script setup>
+useHead({
+  title: 'Resume',
+})
+</script>
+
+<template>
+  <article class="resume active" data-page="resume">
+    <header>
+      <h2 class="h2 article-title">
+        {{ $t('pageTitles.resume') }}
+      </h2>
+    </header>
+
+    <section class="timeline">
+      <ol class="timeline-list">
+        <li class="timeline-item">
+          <h4 class="h4 timeline-item-title">
+            Company
+          </h4>
+          <span>From — Until</span>
+          <p class="timeline-item-desc">
+            Location
+          </p>
+          <p class="timeline-text">
+            Position
+          </p>
+        </li>
+        
+        <li class="timeline-item">
+          <h4 class="h4 timeline-item-title">
+            Company
+          </h4>
+          <span>From — Until</span>
+          <p class="timeline-item-desc">
+            Location
+          </p>
+          <p class="timeline-text">
+            Position
+          </p>
+        </li>
+      </ol>
+    </section>
+
+    <section class="skill">
+      <ul class="skills-list content-card">
+        <li class="skills-item">
+          <div class="title-wrapper">
+            <h5 class="h5">
+              Your Skill
+            </h5>
+            <data value="90">90%</data>
+          </div>
+
+          <div class="skill-progress-bg">
+            <div class="skill-progress-fill" style="width: 90%;" />
+          </div>
+        </li>
+
+      </ul>
+    </section>
+  </article>
+</template>
