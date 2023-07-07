8a78d1d409504997a244426339c15398184a9ca4
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-07-07 09:37:16 +0200

    Squashed commit of the following:
    commit 4ca83447faf91c202a8c8388961efe95bb62b19c
    Merge: 3c7fbec b00336d
    Author: Zachary Cauchi <zach.cauchi@hotmail.com>
    Date:   Thu Jul 6 23:56:08 2023 +0200
    
        Merge pull request #14 from zachary-cauchi/revert-13-feature/skill-categories
    
        Revert "Add skill categories to Resume page"
    
    commit b00336d764285612f1777e3a94d10abd64511991
    Author: Zachary Cauchi <zach.cauchi@hotmail.com>
    Date:   Thu Jul 6 23:55:53 2023 +0200
    
        Revert "Add skill categories to Resume page"
    
    commit 3c7fbecaf21f2d0369bd8d34b664813031abd3fe
    Merge: 9ea8c1c f3e2ca4
    Author: Zachary Cauchi <zach.cauchi@hotmail.com>
    Date:   Thu Jul 6 23:55:42 2023 +0200
    
        Merge pull request #13 from zachary-cauchi/feature/skill-categories
    
        Add skill categories to Resume page

--- server/api/about.js
+++ server/api/about.js
@@ -3,14 +3,11 @@ import { defineEventHandler } from 'h3'
 const en = {
   about: `Hello!
   I'm a result-driven person who aims to deliver exceptional work.
-  I am known for my adaptability to new projects, problem-solving skills in quality assurance testing, quality output, approachability, and enthusiasm.`,
-  hobbies: `I have a fascination for the various computer systems - both hardware and software - that have existed throughout the ages.
+  I am known for my adaptability to new projects, problem-solving skills in quality assurance testing, quality output, approachability, and problem-solving.`,
+  hobbies: `I have a fascination for the various computer systems - both hardware and software - throughout the ages.
   I enjoy exploring and learning about old technologies and recently deprecated ones.
-  That doesn't stop me from exploring new ones though, and I always make it a point to brush up on the current comings and goings of programming tech.
   During my free time, I work on various software projects and enjoy challenging myself to learn new things; you never stop learning after all.`,
-  forYou: `Given a goal, a project, and/or a company vision, I will make it my own and work tirelessly to see it reach its maximum potential.
-  I will always prioritise quality work over easy work and will research the best ways to see a task done before committing it to practice.
-  Above all else, I will always maintain transparent communication with my team and superiors, and deliver my work in a timely fashion and within deadline.`
+  forYou: `Given a goal, a project, and/or a company vision, I will make it my own and work tirelessly to see it reach its maximum potential.`
 }
 
 export default defineEventHandler(() => {
