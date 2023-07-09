0ca2f5f8f39d306d0ae10bfd57e30e447bd9984f
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-07-09 10:03:38 +0200

    feat: force the clicks

--- take-snapshots.ts
+++ take-snapshots.ts
@@ -8,18 +8,18 @@ const url = require('url');
   const baseUrl = process.env.NUXT_PUBLIC_SITE_URL ?? 'http://localhost:3000';
   
   await page.goto(baseUrl, { waitUntil: 'networkidle' });
-  await page.getByTestId(/cookie-dismiss/).click();
+  await page.getByTestId(/cookie-dismiss/).click({ force: true });
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
   await percySnapshot(page, 'About Me Page');
 
 
   await page.goto(new URL('/resume', baseUrl).toString(), { waitUntil: 'networkidle' });
-  await page.getByTestId(/cookie-dismiss/).click();
+  await page.getByTestId(/cookie-dismiss/).click({ force: true });
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
   await percySnapshot(page, 'Resume Page');
   
   await page.goto(new URL('/portfolio', baseUrl).toString(), { waitUntil: 'networkidle' });
-  await page.getByTestId(/cookie-dismiss/).click();
+  await page.getByTestId(/cookie-dismiss/).click({ force: true });
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
   await percySnapshot(page, 'Portfolio Page');
 
