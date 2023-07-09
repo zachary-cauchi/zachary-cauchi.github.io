50dc5607a09ab1e232ba5ddb094ffde5c6d75cbd
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-07-09 09:58:36 +0200

    fix: cookie dismissal twice on page

--- take-snapshots.ts
+++ take-snapshots.ts
@@ -8,22 +8,19 @@ const url = require('url');
   const baseUrl = process.env.NUXT_PUBLIC_SITE_URL ?? 'http://localhost:3000';
   
   await page.goto(baseUrl, { waitUntil: 'networkidle' });
-  
   await page.getByTestId(/cookie-dismiss/).click();
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
-
   await percySnapshot(page, 'About Me Page');
 
+
+  await page.goto(new URL('/resume', baseUrl).toString(), { waitUntil: 'networkidle' });
   await page.getByTestId(/cookie-dismiss/).click();
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
-
-  await page.goto(new URL('/resume', baseUrl).toString());
   await percySnapshot(page, 'Resume Page');
-
+  
+  await page.goto(new URL('/portfolio', baseUrl).toString(), { waitUntil: 'networkidle' });
   await page.getByTestId(/cookie-dismiss/).click();
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
-  
-  await page.goto(new URL('/portfolio', baseUrl).toString());
   await percySnapshot(page, 'Portfolio Page');
 
   await browser.close();
