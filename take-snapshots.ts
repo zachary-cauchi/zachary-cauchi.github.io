eb2b37c2570fda9a2335c88a1902a250527486cc
Zachary Cauchi <zach.cauchi@hotmail.com>
2023-07-09 09:53:44 +0200

    feat: update cookie dismissal

--- take-snapshots.ts
+++ take-snapshots.ts
@@ -9,14 +9,20 @@ const url = require('url');
   
   await page.goto(baseUrl, { waitUntil: 'networkidle' });
   
-  await page.getByTestId(/cookie-dismiss/).click({ force: true });
+  await page.getByTestId(/cookie-dismiss/).click();
   await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
 
   await percySnapshot(page, 'About Me Page');
 
+  await page.getByTestId(/cookie-dismiss/).click();
+  await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
+
   await page.goto(new URL('/resume', baseUrl).toString());
   await percySnapshot(page, 'Resume Page');
 
+  await page.getByTestId(/cookie-dismiss/).click();
+  await expect(page.getByTestId(/cookie-dismiss/)).not.toBeVisible()
+  
   await page.goto(new URL('/portfolio', baseUrl).toString());
   await percySnapshot(page, 'Portfolio Page');
 
